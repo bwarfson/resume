a068d81efadab215690bb70dd87956f95fc7978a
bwarfson <wight.brad@gmail.com>
2014-06-16 18:00:07 -0600

    first commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,31 @@
  * # MainCtrl
  * Controller of the resumeApp
  */
-angular.module('resumeApp')
-  .controller('MainCtrl', function ($scope) {
+  app.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.data = {
+        header: {
+            name:'Brad  Wight',
+            title: 'Web Developer',
+            bio: '',
+            phone: '801-201-2736',
+            website: 'bradwight.com',
+            url: 'http://bradwight.com',
+            email: 'wight.brad@gmail.com',
+            location: 'Lehi, UT',
+            resumeDownload: '',
+            image:''
+        },
+        skills: {},
+        education: {},
+        experience: {},
+        timeline:{},
+        awards:{},
+        interests:{}
+    };
   });
