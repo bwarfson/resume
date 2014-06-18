01dab11af9b67d676545ab8b43893a429cf27894
bwarfson <wight.brad@gmail.com>
2014-06-17 19:36:30 -0600

    first commit

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,3 +1,29 @@
 /**
  * Created by Brad on 6/17/2014.
  */
+
+app.controller('MainCtrl', function($scope){
+    $scope.data = {
+        header: {
+            name:'Brad  Wight',
+            title: 'Web Developer',
+            bio: '',
+            phone: '801-201-2736',
+            website: 'bradwight.com',
+            url: 'http://bradwight.com',
+            email: 'wight.brad@gmail.com',
+            location: 'Lehi, UT',
+            twitter: '',
+            facebook: '',
+            google: '',
+            linkedin: 'bradwight',
+            image:''
+        },
+        skills: {},
+        education: {},
+        experience: {},
+        timeline:{},
+        awards:{},
+        interests:{}
+    };
+});
